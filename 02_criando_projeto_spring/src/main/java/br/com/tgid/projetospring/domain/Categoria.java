f25990b7a9cdc2a08e5750aee3f61685800b130e
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-11 14:06:39 -0300

    criando-BD-H2

--- 02_criando_projeto_spring/src/main/java/br/com/tgid/projetospring/domain/Categoria.java
+++ 02_criando_projeto_spring/src/main/java/br/com/tgid/projetospring/domain/Categoria.java
@@ -3,16 +3,22 @@ package br.com.tgid.projetospring.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // Serializable - Serve para que os objetos possam ser convetidos em uma sequência de bytes.
 //                Desse modo, podendo ser gravados em arquivos e trafegar pela re.de
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	
-
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
