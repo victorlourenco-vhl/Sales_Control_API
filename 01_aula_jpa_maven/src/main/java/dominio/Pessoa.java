b071f6d1477d1da2c66baaac1fd98b3077198408
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-15 16:42:47 -0300

    localizando-objetos-na-BD

--- 01_aula_jpa_maven/src/main/java/dominio/Pessoa.java
+++ 01_aula_jpa_maven/src/main/java/dominio/Pessoa.java
@@ -1,20 +1,30 @@
 package dominio;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Pessoa{
+public class Pessoa implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
 	
+	
+	public Pessoa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public Pessoa(Integer id, String nome, String email) {
+		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
