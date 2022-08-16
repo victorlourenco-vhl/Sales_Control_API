31af9cbd7dbc39e4316e982cef3cc769b5aa777d
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-16 19:03:08 -0300

    Ajuste no endpoint /categorias/{id}

--- 02_projeto_spring/src/main/java/com/tgid/spring/services/CategoriaService.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tgid.spring.domain.Categoria;
 import com.tgid.spring.repositories.CategoriaRepository;
+import com.tgid.spring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tip: " + Categoria.class.getName());
+		}
 		return obj.orElse(null);
 	}
 	
