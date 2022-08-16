4097804b6f23e77021117478f0dc775f7bdce630
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-16 15:03:58 -0300

    Produto e associacao muitos para muitos

--- 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.tgid.spring.domain.Categoria;
+import com.tgid.spring.domain.Produto;
 import com.tgid.spring.repositories.CategoriaRepository;
+import com.tgid.spring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner{
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -26,10 +30,19 @@ public class Application implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- 02_projeto_spring/src/main/java/com/tgid/spring/domain/Categoria.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.tgid.spring.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -17,6 +20,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -42,6 +48,15 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
