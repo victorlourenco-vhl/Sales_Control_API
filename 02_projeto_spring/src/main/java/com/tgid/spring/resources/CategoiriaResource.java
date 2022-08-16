3c31886ccd97a22d08ed301357be55c282fdd8b4
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-16 08:30:06 -0300

    Classe de dominio testada

--- 02_projeto_spring/src/main/java/com/tgid/spring/resources/CategoiriaResource.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/resources/CategoiriaResource.java
@@ -1,17 +1,30 @@
 package com.tgid.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tgid.spring.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoiriaResource {
 	
-	@RequestMapping(value="mensage", method=RequestMethod.GET)
-	public @ResponseBody String listar() {
-		return "REST está funcionando";
+	@RequestMapping(value="/lista", method=RequestMethod.GET)
+	public @ResponseBody List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
