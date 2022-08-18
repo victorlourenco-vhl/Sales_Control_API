01106201cd3bccc938b433483b5fd13867c73319
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-18 10:45:35 -0300

    Listando todas as categorias com DTO

--- 02_projeto_spring/src/main/java/com/tgid/spring/resources/exceptions/ResourceExceptionHandler.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.tgid.spring.services.exceptions.DataIntegrityException;
 import com.tgid.spring.services.exceptions.ObjectNotFoundException;
@@ -19,7 +20,7 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+	@GetMapping
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		
--- 02_projeto_spring/src/main/java/com/tgid/spring/services/CategoriaService.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.tgid.spring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +43,10 @@ public class CategoriaService {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e){
 			throw new DataIntegrityException("Não é possível exculir uma categoria que possui produtos!");
-		}
-		
+		}	
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
