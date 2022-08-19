66ef4392bc669a222e0bf31a6e3a1eefc96fe3d1
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-19 13:41:32 -0300

    Inserindo um novo Cliente com POST

--- 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
@@ -101,7 +101,7 @@ public class Application implements CommandLineRunner{
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		Endereco e1 = new Endereco(null, "Rua Flore", "300", "Apto 303", "Jardim", "38330834", cli1, c1);
-		Endereco e2 = new Endereco(null, "Rua Flore", "300", "Apto 303", "Jardim", "38330834", cli1, c2);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
