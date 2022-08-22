5bfb8ebd533ff48b3c7eda09fc9f08b2a41ce892
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-22 11:28:11 -0300

    Validacao customizada: email nao repetido na validacao de cliente

--- 02_projeto_spring/src/main/java/com/tgid/spring/resources/dto/ClienteDTO.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/resources/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.tgid.spring.domain.Cliente;
+import com.tgid.spring.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
