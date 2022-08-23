5eb37624a2a30fc44ed9f32d927f559b3f47f012
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-23 14:07:27 -0300

    Criando profile de desenvolvimento

---
+++ 02_projeto_spring/src/main/java/com/tgid/spring/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.tgid.spring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.expression.ParseException;
+
+import com.tgid.spring.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException, java.text.ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
