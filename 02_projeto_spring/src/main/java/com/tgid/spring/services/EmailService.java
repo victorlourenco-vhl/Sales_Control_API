e927e476bd9a982cbffefae1654d6c6895875e33
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-24 13:41:05 -0300

    Enviando email com HTML

--- 02_projeto_spring/src/main/java/com/tgid/spring/services/EmailService.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.tgid.spring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.tgid.spring.domain.Pedido;
@@ -8,5 +10,8 @@ public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 	
 }
