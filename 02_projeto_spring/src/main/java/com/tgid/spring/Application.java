96710e3b21f63037136418fb7c4fe97d31bfca18
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-25 17:36:15 -0300

    Enviando imagem via endpoint

--- 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
@@ -1,17 +1,11 @@
 package com.tgid.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.tgid.spring.services.S3Service;
-
 @SpringBootApplication
 public class Application implements CommandLineRunner{
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String [] args) {
 		SpringApplication.run(Application.class, args);
@@ -19,7 +13,7 @@ public class Application implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\temp\\fotos\\foto.jpg");
+		
 	}
 	
 
