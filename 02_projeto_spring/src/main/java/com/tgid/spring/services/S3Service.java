db8de034c04dfaceee7df15992613f673d182488
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-25 17:49:41 -0300

    Tratando exceções adequadamamente

--- 02_projeto_spring/src/main/java/com/tgid/spring/services/S3Service.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.tgid.spring.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -33,7 +34,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 	}
 
@@ -44,11 +45,10 @@ public class S3Service {
 			LOG.info("Iniciando upload");
 			s3client.putObject(bucketName, fileName, is, meta);
 			LOG.info("Upload finalizado");
-		
-		return s3client.getUrl(bucketName, fileName).toURI();
+
+			return s3client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new FileException("Erro ao converter URL para URI");
 		}
-		catch(URISyntaxException e)
-			{
-				throw new RuntimeException("Erro ao converter URL para URI");
-			}
-}}
+	}
+}
