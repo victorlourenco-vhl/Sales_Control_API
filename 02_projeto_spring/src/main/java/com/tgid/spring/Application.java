3279be3c82bfcd1b01f7961fd58a2d343887827d
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-25 15:53:48 -0300

    Salvando primeiro arquivo no S3

--- 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/Application.java
@@ -1,19 +1,25 @@
 package com.tgid.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tgid.spring.services.S3Service;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String [] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\temp\\fotos\\foto.jpg");
 	}
 	
 
