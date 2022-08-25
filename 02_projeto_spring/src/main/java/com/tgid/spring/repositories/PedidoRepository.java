8bf834c839c9681be8d641e5dfb309921e0413e2
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-25 14:01:27 -0300

    Restricao de conteudo: Cliente so recupera pedidos

--- 02_projeto_spring/src/main/java/com/tgid/spring/repositories/PedidoRepository.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/repositories/PedidoRepository.java
@@ -1,13 +1,18 @@
 package com.tgid.spring.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tgid.spring.domain.Cliente;
 import com.tgid.spring.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	 
-	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 	
 }
--- 02_projeto_spring/src/main/java/com/tgid/spring/services/PedidoService.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.tgid.spring.domain.Cliente;
 import com.tgid.spring.domain.ItemPedido;
 import com.tgid.spring.domain.PagamentoComBoleto;
 import com.tgid.spring.domain.Pedido;
@@ -13,6 +17,8 @@ import com.tgid.spring.domain.enums.EstadoPagamento;
 import com.tgid.spring.repositories.ItemPedidoRepository;
 import com.tgid.spring.repositories.PagamentoRepository;
 import com.tgid.spring.repositories.PedidoRepository;
+import com.tgid.spring.security.UserSS;
+import com.tgid.spring.services.exceptions.AuthorizationException;
 import com.tgid.spring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,6 +76,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 	
 }
