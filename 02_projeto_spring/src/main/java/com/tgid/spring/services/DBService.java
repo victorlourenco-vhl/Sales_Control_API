9e8390eae9e2f20ee4ba5b8b963d16e0ee7f1e2b
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-25 11:07:24 -0300

    Implementando a autenticacao e geracao do token JWT

--- 02_projeto_spring/src/main/java/com/tgid/spring/services/DBService.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/services/DBService.java
@@ -114,12 +114,12 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nelio.cursos@gmail.com", "36378912377",
+		Cliente cli1 = new Cliente(null, "Maria Silva", "victor@gmail.com", "36378912377",
 				TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "vhl.12@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
 		cli2.addPerfil(Perfil.ADMIN);
 
