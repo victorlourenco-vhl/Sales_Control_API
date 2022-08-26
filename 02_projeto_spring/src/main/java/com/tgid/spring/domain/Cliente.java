d20a963b574f96c8bf2ed54c8131520effc6c30f
Victor Lourenço <victorhlo12@hotmail.com>
2022-08-26 09:19:07 -0300

    Usando padrao de nomes para imagens

--- 02_projeto_spring/src/main/java/com/tgid/spring/domain/Cliente.java
+++ 02_projeto_spring/src/main/java/com/tgid/spring/domain/Cliente.java
@@ -58,9 +58,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -157,15 +154,6 @@ public class Cliente implements Serializable{
 	public void addPerfil(Perfil perfil) {
 		perfis.add(perfil.getCod());
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	
 	
 	@Override
 	public int hashCode() {
